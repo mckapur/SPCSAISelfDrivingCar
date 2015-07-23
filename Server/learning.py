f8c037415c6b23d1dd0c6e25e9b80ff770ee3c29
Rohan Kapur <me@rohankapur.com>
2015-07-23 08:47:35 -0700

    Fixed bugs and added/removed methods

--- Server/learning.py
+++ Server/learning.py
@@ -11,24 +11,22 @@ import numpy as np
 
 def sigmoid(x):
     return 1.0/(1.0 + np.exp(-x))
-
-def sigmoid_prime(x):
+def dsigmoid(x):
     return x*(1.0-x)
 
 def tanh(x):
     return np.tanh(x)
-
-def tanh_prime(x):
+def dtanh(x):
     return 1.0 - x**2
 
 class NeuralNetwork:
     def __init__(self, layers, weights=[], activation='sigmoid'):
         if activation == 'sigmoid':
             self.activation = sigmoid
-            self.activation_prime = sigmoid_prime
+            self.dactivation = dsigmoid
         elif activation == 'tanh':
             self.activation = tanh
-            self.activation_prime = tanh_prime
+            self.dactivation = dtanh
         self.weights = weights
         if (len(self.weights) != layers[0]):
             for i in range(1, len(layers) - 1):
@@ -45,9 +43,9 @@ class NeuralNetwork:
                     activation = self.activation(dot_value)
                     a.append(activation)
             error = y[i] - a[-1]
-            deltas = [error * self.activation_prime(a[-1])]
+            deltas = [error * self.dactivation(a[-1])]
             for l in range(len(a) - 2, 0, -1): 
-                deltas.append(deltas[-1].dot(self.weights[l].T) * self.activation_prime(a[l]))
+                deltas.append(deltas[-1].dot(self.weights[l].T) * self.dactivation(a[l]))
             deltas.reverse()
             for i in range(len(self.weights)):
                 layer = np.atleast_2d(a[i])
@@ -67,11 +65,11 @@ WEIGHTS_DUMP_FILENAME = "_weights_"
 DATA_DUMP_SUFFIX = "dump.npy"
 
 class PersistanceManager:
-    def __init__(namespace):
+    def __init__(self, namespace):
         self.namespace = namespace
     def relPathFromFilename(self, filename):
         return DATA_DUMP_DIRECTORY + filename + self.namespace + DATA_DUMP_SUFFIX
-    def persistData(X=[], y=[], weights=[]):
+    def persistData(self, X=[], y=[], weights=[]):
         np.save(self.relPathFromFilename(INPUT_X_DUMP_FILENAME), X)
         np.save(self.relPathFromFilename(INPUT_y_DUMP_FILENAME), y)
         np.save(self.relPathFromFilename(WEIGHTS_DUMP_FILENAME), weights)
@@ -85,12 +83,15 @@ class PersistanceManager:
 
 class AbstractLearningClient:
     def __init__(self, name):
-        self.persistanceManager = PersistanceManager(name)
+        self.restoreFromPersistance()
+        self.configureNeuralNetwork(False)
+    def restoreFromPersistance(self):
+        if not self.persistanceManager:
+            self.persistanceManager = PersistanceManager(name)
         data = self.persistanceManager.getPersistedData()
         self.X = data['X']
         self.y = data['y']
         self.weights = data['weights']
-        self.configureNeuralNetwork(False)
     def configureNeuralNetwork(self, shouldTrain):
         inputColumnSize = self.X.shape[1]
         hiddenColumnSize = inputColumnSize
@@ -106,10 +107,11 @@ class AbstractLearningClient:
         self.y = np.append(self.y, y)
         self.configureNeuralNetwork(True)
         self.persistanceManager.persistData(self.X, self.y, self.weights)
-    def output(X):
+    def output(self, X):
         self.net.predict(X)
     def wipe(self):
-        self.wipePersistedData
+        self.persistanceManager.wipePersistedData()
+        self.restoreFromPersistance()
         return True
 
 MOTION_HANDLER_NAME = 'MOTION_HANDLER'
@@ -118,8 +120,7 @@ class MotionHandler:
     def __init__(self):
         self.learningClient = AbstractLearningClient(MOTION_HANDLER_NAME)
     def receivedNewMotionData(self, data):
-    def endedReceivingMotionData(self):
     def reinforceMotionData(self, data):
-    def suggestedMotionResponseFromData(data):
-    def delete:
+    def suggestedMotionResponseFromData(self, data):
+    def delete(self):
         return self.learningClient.wipe()
