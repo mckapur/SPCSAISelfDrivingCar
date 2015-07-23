221455484967baa12f65f6f7a328bb877f2eef78
Rohan Kapur <me@rohankapur.com>
2015-07-23 00:35:32 -0700

    Added persistance and some learning client code

--- Server/learning.py
+++ Server/learning.py
@@ -22,21 +22,22 @@ def tanh_prime(x):
     return 1.0 - x**2
 
 class NeuralNetwork:
-    def __init__(self, layers, activation='sigmoid'):
+    def __init__(self, layers, weights=[], activation='sigmoid'):
         if activation == 'sigmoid':
             self.activation = sigmoid
             self.activation_prime = sigmoid_prime
         elif activation == 'tanh':
             self.activation = tanh
             self.activation_prime = tanh_prime
-        self.weights = []
-        for i in range(1, len(layers) - 1):
-            self.weights.append(2*np.random.random((layers[i-1]+1, layers[i]+1))-1)
-        self.weights.append(2*np.random.random((layers[i]+1, layers[i+1]))-1)
-    def train(self, X, y, learning_rate=0.2, epochs=100000):
+        self.weights = weights
+        if (len(self.weights) != layers[0]):
+            for i in range(1, len(layers) - 1):
+                self.weights.append(2 * np.random.random((layers[i-1] + 1, layers[i] + 1)) - 1)
+            self.weights.append(2 * np.random.random((layers[i] + 1, layers[i+1])) - 1)
+    def train(self, X, y, learning_rate=0.2, iters=100000):
         ones = np.atleast_2d(np.ones(X.shape[0]))
         X = np.concatenate((ones.T, X), axis=1)
-        for k in range(epochs):
+        for k in range(iters):
             i = np.random.randint(X.shape[0])
             a = [X[i]]
             for l in range(len(self.weights)):
@@ -46,20 +47,50 @@ class NeuralNetwork:
             error = y[i] - a[-1]
             deltas = [error * self.activation_prime(a[-1])]
             for l in range(len(a) - 2, 0, -1): 
-                deltas.append(deltas[-1].dot(self.weights[l].T)*self.activation_prime(a[l]))
+                deltas.append(deltas[-1].dot(self.weights[l].T) * self.activation_prime(a[l]))
             deltas.reverse()
             for i in range(len(self.weights)):
                 layer = np.atleast_2d(a[i])
                 delta = np.atleast_2d(deltas[i])
                 self.weights[i] += learning_rate * layer.T.dot(delta)
-            if k % 10000 == 0: print 'epochs:', k
+            if k % 10000 == 0: print 'iters:', k
     def predict(self, x):
         a = np.concatenate((np.ones(1).T, np.array(x)), axis=1)      
         for l in range(0, len(self.weights)):
             a = self.activation(np.dot(a, self.weights[l]))
         return a
 
-nn = NeuralNetwork([2, 2, 1])
+DATA_DUMP_DIRECTORY = "data_dump"
+INPUT_X_DUMP_FILENAME = "input_X_dump.npy"
+INPUT_y_DUMP_FILENAME = "input_y_dump.npy"
+WEIGHTS_DUMP_FILENAME = "weights_dump.npy"
+
+class PersistanceManager:
+    def persistData(X=[], y=[], weights=[]):
+        np.save(DATA_DUMP_DIRECTORY + INPUT_X_DUMP_FILENAME, X)
+        np.save(DATA_DUMP_DIRECTORY + INPUT_y_DUMP_FILENAME, y)
+        np.save(DATA_DUMP_DIRECTORY + WEIGHTS_DUMP_FILENAME, weights)
+    def getPersistedData(self):
+        X = np.load(DATA_DUMP_DIRECTORY + INPUT_X_DUMP_FILENAME)
+        y = np.load(DATA_DUMP_DIRECTORY + INPUT_y_DUMP_FILENAME)
+        weights = np.load(DATA_DUMP_DIRECTORY + WEIGHTS_DUMP_FILENAME)
+        return {'X': X, 'y': y, 'weights': weights}
+    def wipePersistedData(self):
+        self.persistData()
+
+class LearningClient:
+    def __init__(self):
+        self.persistanceManager = PersistanceManager()
+        data = self.persistanceManager.getPersistedData();
+        self.X = data['X']
+        self.y = data['y']
+        self.weights = data['weights']
+    def beginInputStream(self):
+        # Append
+    def endInputStream(self):
+        # Train on X/y and persist
+
+nn = NeuralNetwork([2, 2, 2])
 X = np.array([
 	[0, 0], 
 	[0, 1], 
